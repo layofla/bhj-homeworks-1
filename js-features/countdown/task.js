3c234a0dadb57253ca51ed3daa2b950cc830b60b
Asya Douglas <asya.douglas17@gmail.com>
2022-12-02 18:49:16 +0800

    change

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,12 @@
 const countTime  = function(){
- const output = document.getElementById("timer");
-  if (output.textContent >= 1){
-        output.textContent --;
-    } else if(output.textContent = '0'){
-        alert("Вы победили в конкурсе!");
-    }
-}
-
-setInterval(countTime,1000);
+    const output = document.getElementById("timer");
+     if (output.textContent >= 1){
+           output.textContent --;
+       } else if(output.textContent = '0'){
+           alert("Вы победили в конкурсе!");
+       }
+   }
+   
+   setInterval(countTime,1000);
+   
+   
