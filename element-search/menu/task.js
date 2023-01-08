8c69663ecaefb53e4be53363d0738618d08eb73d
Asya Douglas <asya.douglas17@gmail.com>
2023-01-08 16:41:07 +0800

    task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,17 @@
-const links = Array.from(document.querySelectorAll('.menu__link'));
+const element = Array.from(document.querySelectorAll('.menu__link'));
 
-for (let link of links) {
-  link.onclick = function () {
-  const parent = link.parentElement;
+for (let link of element) {
+	link.onclick = function () {
+		const parent = link.parentElement;
 
-if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
-   parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
-}  else {
-   arent.querySelector('.menu_sub').className = 'menu menu_sub'
- }
+		if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+			parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+		} else {
+			parent.querySelector('.menu_sub').className = 'menu menu_sub'
+		}
 
-if (link.closest('.menu_main')) {
-return false
-  }
- }
+		if (link.closest('.menu_main')) {
+			return false
+		}
+	}
 }
